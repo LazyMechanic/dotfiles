2de9b32d952a56f853febecff472edab1398b6f6
lazymechanic <asharnrus@gmail.com>
2020-03-21 15:06:34 +0300

    Adds picom

---
+++ dotfiles/.config/i3/picom.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+# If picom is running, kill it to prevent multiple instances
+if ps -A | grep picom; then
+    killall -q picom
+fi
+
+# Load picom
+picom --experimental-backends --backend glx
