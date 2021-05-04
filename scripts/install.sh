b2bea8e1d354701715484a21fbe6dde274f216ea
LazyMechanic <asharnrus@gmail.com>
2021-05-04 16:18:53 +0300

    Separate scripts

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/bash
+
 ######################## UTILS BEGIN ########################
 
 setup_color() {
@@ -104,27 +106,9 @@ yes_no() {
 
 _REMOTE_REPO="https://github.com/LazyMechanic/configs.git"
 _LOCAL_REPO="lazymechanic.configs"
-_ZSH_CUSTOM=""
-_FONTS_DIR="$_LOCAL_REPO/fonts"
-_DOTFILES_DIR="$_LOCAL_REPO/dotfiles"
-
-_INSTALL_ZSH_THEME_SCRIPT="$_LOCAL_REPO/scripts/install-zsh-theme.py"
 
 ######################## VARIABLES END ########################
 
-check_oh_my_zsh() {  
-    if [[ -z "$_ZSH_CUSTOM" ]];
-    then
-        error "Oh-my-zsh custom themes directory is empty"
-        exit 1
-    fi
-
-    # if [[ ! -d "$_ZSH_CUSTOM" ]];
-    # then
-    #     error "Oh-my-zsh custom themes directory not exists"
-    #     exit 1
-    # fi
-}
 
 clone_project() {
     info "Start clone project..."
@@ -149,208 +133,11 @@ clone_project() {
         "$_REMOTE_REPO" "$_LOCAL_REPO" 
     if [[ ! $? -eq 0 ]];
     then
-        error "git clone of '$_REMOTE_REPO' repo failed"
+        error "Failed"
         exit 1
     fi
     
     ok "Done!"
-}
-
-select_theme() {
-    PS3='Please enter theme: '
-    options=("LazyMechanic" "Powerlevel10k (lean)" "Powerlevel10k (classic)" "Powerlevel10k (rainbow)" "Default (robbyrussell)")
-    select opt in "${options[@]}"
-    do
-        case $opt in
-            "LazyMechanic")
-                echo "lazymechanic"
-                return
-                ;;
-            "Powerlevel10k (lean)")
-                echo "p10klean"
-                return
-                ;;
-            "Powerlevel10k (classic)")
-                echo "p10kclassic"
-                return
-                ;;
-            "Powerlevel10k (rainbow)")
-                echo "p10krainbow"
-                return
-                ;;
-            "Default (robbyrussell)")
-                echo "default"
-                return
-                ;;
-            *) echo "invalid option $REPLY";;
-        esac
-    done
-}
-
-install_zsh_theme() {
-    info "Start install zsh theme..."
-    
-    theme=$(select_theme)
-    python3 \
-        "$_INSTALL_ZSH_THEME_SCRIPT" \
-        --zsh-custom "$_ZSH_CUSTOM" \
-        "$theme" 
-    if [[ ! $? -eq 0 ]];
-    then
-        error "Install zsh theme failed"
-        exit 1
-    fi
-    
-    ok "Done!"
-    info "Start zsh session and call 'source ~/.zshrc'"
-}
-
-install_fonts() {
-    info "Start install fonts..."
-    
-    # Check if need setup custom fonts
-    answer=$(yes_no "Install custom fonts?" "y")
-    if [ "$answer" == "y" ];
-    then
-        local_fonts_dir="$HOME/.local/share/fonts"
-        mkdir -p "$local_fonts_dir"
-        
-        fonts=$(ls "$_FONTS_DIR")
-        info "Font files to be installed:"
-        for f in $fonts
-        do
-            echo " - $f"
-        done
-        
-        info "Install fonts"
-        cp -r "$_FONTS_DIR/." "$local_fonts_dir/"
-        if [[ ! $? -eq 0 ]];
-        then
-            error "Failed"
-            return
-        fi
-
-        ok "Done!"
-    else
-        ok "Do nothing"
-        return
-    fi
-}
-
-install_dotfiles() {
-    info "Install dependencies"
-
-    sudo sh -c 'pacman -Syu'
-    if [[ ! $? -eq 0 ]];
-    then
-        error "Failed"
-        return
-    fi
-
-    sudo sh -c 'pacman -S yay'
-    if [[ ! $? -eq 0 ]];
-    then
-        error "Failed"
-        return
-    fi
-
-    yay -S                          \
-        zsh                         \
-        bat                         \
-        exa                         \
-        feh                         \
-        yad                         \
-        rofi                        \
-        dmenu                       \
-        dunst                       \
-        picom                       \
-        polybar                     \
-        xdotool                     \
-        autorandr                   \
-        playerctl                   \
-        lxappearance-gtk3           \
-        aur/i3-gaps-next-git        \
-        extra/xorg-xbacklight       \
-        aur/zsh-autosuggestions-git
-    if [[ ! $? -eq 0 ]];
-    then
-        error "Failed"
-        return
-    fi
-
-    ok "Done!"
-
-    info "Install oh-my-zsh"
-    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
-    if [[ ! $? -eq 0 ]];
-    then
-        error "Failed"
-        return
-    fi
-    ok "Done!"
-
-    info "Copy dotfiles"
-
-    local_dfiles_dir="$HOME"       
-    cp -r "$_DOTFILES_DIR/." "$local_dfiles_dir/"
-    if [[ ! $? -eq 0 ]];
-    then
-        error "Failed"
-        return
-    fi
-    ok "Done!"
-
-    info "Don\'t forget to change shell by \'chsh -s $(which zsh)\'"
-
-    return
-}
-
-select_action() {
-    PS3='Please enter action: '
-    options=("Install zsh theme" "Install fonts" "Install dotfiles" "Exit")
-    select opt in "${options[@]}"
-    do
-        case $opt in
-            "Install zsh theme")
-                echo "zsh"
-                return
-                ;;
-            "Install fonts")
-                echo "fonts"
-                return
-                ;;
-            "Install dotfiles")
-                echo "dotfiles"
-                return
-                ;;
-            "Exit")
-                echo "exit"
-                return
-                ;;
-            *) echo "invalid option $REPLY";;
-        esac
-    done
-}
-
-main_loop() {
-    while true
-    do
-        action=$(select_action)
-        case $action in
-            "zsh")
-                install_zsh_theme
-                ;;
-            "fonts")
-                install_fonts
-                ;;
-            "dotfiles")
-                install_dotfiles
-                ;;
-            "exit")
-                return
-                ;;
-        esac
-    done
 }
 
 remove_project() {
@@ -366,15 +153,12 @@ remove_project() {
     fi
 }
 
+
 main() {
     setup_color
 
     # Check commands
     check_command git
-    check_command sed
-    check_command source
-    check_command python3
-    check_command pacman
     
     # If no has argument
     if [[ $# != 1 ]];
@@ -384,11 +168,8 @@ main() {
 
     _ZSH_CUSTOM="$1"
 
-    # Check if zsh custom dir exists
-    check_oh_my_zsh
-
     clone_project
-    main_loop
+    bash -c "$_LOCAL_REPO/scripts/install-impl.sh" "" $1
     remove_project
 }
 
